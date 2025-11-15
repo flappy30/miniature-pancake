9000baf76a4bdd8f84da4ee5859219f1b8fbf12d
flappy30 <yaroslav300308@gmailcom>
2025-11-15 22:50:15 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+var a float64
+var b float64
+var action string
+
+func main() {
+	calculator()
+}
+
+func calculator() {
+	fmt.Print("Введите первое число... ")
+	fmt.Scan(&a)
+	fmt.Print("Введите знак действия... ")
+	fmt.Scan(&action)
+	fmt.Print("Введите второе число... ")
+	fmt.Scan(&b)
+	fmt.Println("")
+
+	switch action {
+	case "+":
+		amount(a,b)
+	case "-":
+		subtraction(a,b)
+	case "*":
+		multiply(a,b)
+	case "/":
+		divide(a,b)
+	}
+}
+
+func amount(a float64, b float64) {
+	sum := a+b
+	fmt.Println("Ответ ")
+	fmt.Printf("%.2f", sum)
+}
+
+func subtraction(a float64, b float64) {
+	sub := a-b
+	fmt.Println("Ответ ")
+	fmt.Printf("%.2f", sub)
+}
+
+func multiply(a float64, b float64) {
+	mul := a*b
+	fmt.Println("Ответ ")
+	fmt.Printf("%.2f", mul)
+}
+
+func divide(a float64, b float64) {
+	div := a/b
+	fmt.Println("Ответ ")
+	fmt.Printf("%.2f", div)
+}
